be6810ef8dfcb84cfd02010eb94ab050ad65b501
THOTA DHANA SEKHAR <97746771+THOTA-DHANASEKHAR@users.noreply.github.com>
2023-10-28 11:13:30 +0530

    Update script.js

--- MyScript/script.js
+++ MyScript/script.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
 	function checkScreenSize() {
 	const listofitems = `
       <div id='listofitems'>
-<li class='LeftSideIndex_Btn' id='SQL_Tutorial' name = 'SQL_Tutorial'><a href='#SQL_Tutorial'>SQL Tutorial</a></li>
+<li class='LeftSideIndex_Btn_h' id='SQL_Tutorial' name = 'SQL_Tutorial'><a href='#SQL_Tutorial'>SQL Tutorial</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_HOME' name = 'SQL_HOME'><a href='#SQL_HOME'>SQL HOME</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Intro' name = 'SQL_Intro'><a href='#SQL_Intro'>SQL Intro</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Syntax' name = 'SQL_Syntax'><a href='#SQL_Syntax'>SQL Syntax</a></li>
@@ -86,6 +86,8 @@ $(document).ready(function() {
 <li class='LeftSideIndex_Btn' id='SQL_And' name = 'SQL_And'><a href='#SQL_And'>SQL And</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Or' name = 'SQL_Or'><a href='#SQL_Or'>SQL Or</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Not' name = 'SQL_Not'><a href='#SQL_Not'>SQL Not</a></li>
+
+<li class='LeftSideIndex_Btn_h' id='SQL_Not1' name = 'SQL_Not'><a href='#SQL_Not'>SQL Joins</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Insert_Into' name = 'SQL_Insert_Into'><a href='#SQL_Insert_Into'>SQL Insert Into</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Null_Values' name = 'SQL_Null_Values'><a href='#SQL_Null_Values'>SQL Null Values</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Update' name = 'SQL_Update'><a href='#SQL_Update'>SQL Update</a></li>
@@ -95,6 +97,8 @@ $(document).ready(function() {
 <li class='LeftSideIndex_Btn' id='SQL_Count' name = 'SQL_Count'><a href='#SQL_Count'>SQL Count</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Sum' name = 'SQL_Sum'><a href='#SQL_Sum'>SQL Sum</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Avg' name = 'SQL_Avg'><a href='#SQL_Avg'>SQL Avg</a></li>
+
+<li class='LeftSideIndex_Btn_h' id='SQL_Not1' name = 'SQL_Not'><a href='#SQL_Not'>SQL Set operators</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Like' name = 'SQL_Like'><a href='#SQL_Like'>SQL Like</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Wildcards' name = 'SQL_Wildcards'><a href='#SQL_Wildcards'>SQL Wildcards</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_In' name = 'SQL_In'><a href='#SQL_In'>SQL In</a></li>
@@ -106,6 +110,9 @@ $(document).ready(function() {
 <li class='LeftSideIndex_Btn' id='SQL_Right_Join' name = 'SQL_Right_Join'><a href='#SQL_Right_Join'>SQL Right Join</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Full_Join' name = 'SQL_Full_Join'><a href='#SQL_Full_Join'>SQL Full Join</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Self_Join' name = 'SQL_Self_Join'><a href='#SQL_Self_Join'>SQL Self Join</a></li>
+
+
+<li class='LeftSideIndex_Btn_h' id='SQL_Not' name = 'SQL_Not'><a href='#SQL_Not'>SQL Aggrigate Functions</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Union' name = 'SQL_Union'><a href='#SQL_Union'>SQL Union</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Group_By' name = 'SQL_Group_By'><a href='#SQL_Group_By'>SQL Group By</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Having' name = 'SQL_Having'><a href='#SQL_Having'>SQL Having</a></li>
@@ -114,6 +121,9 @@ $(document).ready(function() {
 <li class='LeftSideIndex_Btn' id='SQL_Select_Into' name = 'SQL_Select_Into'><a href='#SQL_Select_Into'>SQL Select Into</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Insert_Into_Select' name = 'SQL_Insert_Into_Select'><a href='#SQL_Insert_Into_Select'>SQL Insert Into Select</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Case' name = 'SQL_Case'><a href='#SQL_Case'>SQL Case</a></li>
+
+
+<li class='LeftSideIndex_Btn_h' id='SQL_Not' name = 'SQL_Not'><a href='#SQL_Not'>SQL DML</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Null_Functions' name = 'SQL_Null_Functions'><a href='#SQL_Null_Functions'>SQL Null Functions</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Stored_Procedures' name = 'SQL_Stored_Procedures'><a href='#SQL_Stored_Procedures'>SQL Stored Procedures</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Comments' name = 'SQL_Comments'><a href='#SQL_Comments'>SQL Comments</a></li>
@@ -123,6 +133,9 @@ $(document).ready(function() {
 <li class='LeftSideIndex_Btn' id='SQL_Create_DB' name = 'SQL_Create_DB'><a href='#SQL_Create_DB'>SQL Create DB</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Drop_DB' name = 'SQL_Drop_DB'><a href='#SQL_Drop_DB'>SQL Drop DB</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Backup_DB' name = 'SQL_Backup_DB'><a href='#SQL_Backup_DB'>SQL Backup DB</a></li>
+
+
+<li class='LeftSideIndex_Btn_h' id='SQL_Not' name = 'SQL_Not'><a href='#SQL_Not'>SQL functions</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Create_Table' name = 'SQL_Create_Table'><a href='#SQL_Create_Table'>SQL Create Table</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Drop_Table' name = 'SQL_Drop_Table'><a href='#SQL_Drop_Table'>SQL Drop Table</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Alter_Table' name = 'SQL_Alter_Table'><a href='#SQL_Alter_Table'>SQL Alter Table</a></li>
@@ -134,6 +147,8 @@ $(document).ready(function() {
 <li class='LeftSideIndex_Btn' id='SQL_Check' name = 'SQL_Check'><a href='#SQL_Check'>SQL Check</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Default' name = 'SQL_Default'><a href='#SQL_Default'>SQL Default</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Index' name = 'SQL_Index'><a href='#SQL_Index'>SQL Index</a></li>
+
+<li class='LeftSideIndex_Btn_h' id='SQL_Not' name = 'SQL_Not'><a href='#SQL_Not'>SQL DDL</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Auto_Increment' name = 'SQL_Auto_Increment'><a href='#SQL_Auto_Increment'>SQL Auto Increment</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Dates' name = 'SQL_Dates'><a href='#SQL_Dates'>SQL Dates</a></li>
 <li class='LeftSideIndex_Btn' id='SQL_Views' name = 'SQL_Views'><a href='#SQL_Views'>SQL Views</a></li>
