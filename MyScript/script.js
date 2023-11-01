6267dbd23f8050dd8513396948e3270e7cbe9e89
THOTA DHANA SEKHAR <97746771+THOTA-DHANASEKHAR@users.noreply.github.com>
2023-11-01 23:13:14 +0530

    Update script.js

--- MyScript/script.js
+++ MyScript/script.js
@@ -13,28 +13,28 @@ function copyCodeToClipboard(button) {
 */
 
 function copyCodeToClipboard(button) {
-  var codeElement = button.closest(".code-container").querySelector("code");
+	var codeElement = button.closest(".code-container").querySelector("code");
 
-  if (codeElement) {
-    var textArea = document.createElement("textarea");
-    textArea.value = codeElement.textContent;
-    document.body.appendChild(textArea);
-    textArea.select();
+	if (codeElement) {
+		var textArea = document.createElement("textarea");
+		textArea.value = codeElement.textContent;
+		document.body.appendChild(textArea);
+		textArea.select();
 
-    try {
-      document.execCommand("copy");
-      button.innerHTML = "<i class='fas fa-check'> </i> Copied!";
-      button.classList.add("copied");
-    } catch (err) {
-      console.error("Unable to copy to clipboard: " + err);
-    }
-    document.body.removeChild(textArea);
+		try {
+			document.execCommand("copy");
+			button.innerHTML = "<i class='fas fa-check'> </i> Copied!";
+			button.classList.add("copied");
+		} catch (err) {
+			console.error("Unable to copy to clipboard: " + err);
+		}
+		document.body.removeChild(textArea);
 
-    setTimeout(function () {
-      button.innerHTML = '<i class="fas fa-copy"></i> copy';
-		  button.classList.remove("copied");
-    }, 2000);
-  }
+		setTimeout(function() {
+			button.innerHTML = '<i class="fas fa-copy"></i> copy';
+			button.classList.remove("copied");
+		}, 2000);
+	}
 }
 
 const toggleButtons = document.querySelectorAll('.toggleButton');
@@ -54,12 +54,12 @@ toggleButtons.forEach(button => {
 });
 
 function toggleNav() {
-  var list = document.getElementById("listofitems_mobile");
-  if (list.style.width === "0%" || list.style.width === "") {
-    list.style.width = "50%";
-  } else {
-    list.style.width = "0%";
-  }
+	var list = document.getElementById("listofitems_mobile");
+	if (list.style.width === "0%" || list.style.width === "") {
+		list.style.width = "50%";
+	} else {
+		list.style.width = "0%";
+	}
 }
 
 function openNav() {
@@ -73,91 +73,28 @@ function closeNav() {
 
 $(document).ready(function() {
 	function checkScreenSize() {
-	const listofitems = `
+		const listofitems = `
       <div id='listofitems'>
-<li class='LeftSideIndex_Btn_h' id='SQL_Tutorial' name = 'SQL_Tutorial'><a href='#SQL_Tutorial'>SQL Tutorial</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_HOME' name = 'SQL_HOME'><a href='#SQL_HOME'>SQL HOME</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Intro' name = 'SQL_Intro'><a href='#SQL_Intro'>SQL Intro</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Syntax' name = 'SQL_Syntax'><a href='#SQL_Syntax'>SQL Syntax</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Select' name = 'SQL_Select'><a href='#SQL_Select'>SQL Select</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Select_Distinct' name = 'SQL_Select_Distinct'><a href='#SQL_Select_Distinct'>SQL Select Distinct</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Where' name = 'SQL_Where'><a href='#SQL_Where'>SQL Where</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Order_By' name = 'SQL_Order_By'><a href='#SQL_Order_By'>SQL Order By</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_And' name = 'SQL_And'><a href='#SQL_And'>SQL And</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Or' name = 'SQL_Or'><a href='#SQL_Or'>SQL Or</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Not' name = 'SQL_Not'><a href='#SQL_Not'>SQL Not</a></li>
+<li class='LeftSideIndex_Btn_h' id='SQL BASICS' name = 'SQL BASICS'>SQL SQL BASICS</li>
+<a href='/index.html'><li class='LeftSideIndex_Btn' id='Intro' name = 'Intro'>SQL Intro</li></a>
+<a href='/pros&cons.html'><li class='LeftSideIndex_Btn' id='pros&cons' name = 'pros&cons'>SQL Pros & Cons</li></a>
+<a href='/comments.html'><li class='LeftSideIndex_Btn' id='comments' name = 'comments'>SQL Comments</li></a>
+<a href='/commands.html'><li class='LeftSideIndex_Btn' id='commands' name = 'commands'>SQL Commands</li></a>
+<a href='/case_sensitive.html'><li class='LeftSideIndex_Btn' id='case_sensitive' name = 'case sensitive'>SQL Case Sensitive</li></a>
+<a href='/bestpratices.html'><li class='LeftSideIndex_Btn' id='bestpratices' name = 'Best pratices'>SQL Best Pratices</li></a>
 
-<li class='LeftSideIndex_Btn_h' id='SQL_Not1' name = 'SQL_Not'><a href='#SQL_Not'>SQL Joins</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Insert_Into' name = 'SQL_Insert_Into'><a href='#SQL_Insert_Into'>SQL Insert Into</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Null_Values' name = 'SQL_Null_Values'><a href='#SQL_Null_Values'>SQL Null Values</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Update' name = 'SQL_Update'><a href='#SQL_Update'>SQL Update</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Delete' name = 'SQL_Delete'><a href='#SQL_Delete'>SQL Delete</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Select_Top' name = 'SQL_Select_Top'><a href='#SQL_Select_Top'>SQL Select Top</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Min_and_Max' name = 'SQL_Min_and_Max'><a href='#SQL_Min_and_Max'>SQL Min and Max</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Count' name = 'SQL_Count'><a href='#SQL_Count'>SQL Count</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Sum' name = 'SQL_Sum'><a href='#SQL_Sum'>SQL Sum</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Avg' name = 'SQL_Avg'><a href='#SQL_Avg'>SQL Avg</a></li>
-
-<li class='LeftSideIndex_Btn_h' id='SQL_Not1' name = 'SQL_Not'><a href='#SQL_Not'>SQL Set operators</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Like' name = 'SQL_Like'><a href='#SQL_Like'>SQL Like</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Wildcards' name = 'SQL_Wildcards'><a href='#SQL_Wildcards'>SQL Wildcards</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_In' name = 'SQL_In'><a href='#SQL_In'>SQL In</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Between' name = 'SQL_Between'><a href='#SQL_Between'>SQL Between</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Aliases' name = 'SQL_Aliases'><a href='#SQL_Aliases'>SQL Aliases</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Joins' name = 'SQL_Joins'><a href='#SQL_Joins'>SQL Joins</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Inner_Join' name = 'SQL_Inner_Join'><a href='#SQL_Inner_Join'>SQL Inner Join</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Left_Join' name = 'SQL_Left_Join'><a href='#SQL_Left_Join'>SQL Left Join</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Right_Join' name = 'SQL_Right_Join'><a href='#SQL_Right_Join'>SQL Right Join</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Full_Join' name = 'SQL_Full_Join'><a href='#SQL_Full_Join'>SQL Full Join</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Self_Join' name = 'SQL_Self_Join'><a href='#SQL_Self_Join'>SQL Self Join</a></li>
-
-
-<li class='LeftSideIndex_Btn_h' id='SQL_Not' name = 'SQL_Not'><a href='#SQL_Not'>SQL Aggrigate Functions</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Union' name = 'SQL_Union'><a href='#SQL_Union'>SQL Union</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Group_By' name = 'SQL_Group_By'><a href='#SQL_Group_By'>SQL Group By</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Having' name = 'SQL_Having'><a href='#SQL_Having'>SQL Having</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Exists' name = 'SQL_Exists'><a href='#SQL_Exists'>SQL Exists</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Any,_All' name = 'SQL_Any,_All'><a href='#SQL_Any,_All'>SQL Any, All</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Select_Into' name = 'SQL_Select_Into'><a href='#SQL_Select_Into'>SQL Select Into</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Insert_Into_Select' name = 'SQL_Insert_Into_Select'><a href='#SQL_Insert_Into_Select'>SQL Insert Into Select</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Case' name = 'SQL_Case'><a href='#SQL_Case'>SQL Case</a></li>
-
-
-<li class='LeftSideIndex_Btn_h' id='SQL_Not' name = 'SQL_Not'><a href='#SQL_Not'>SQL DML</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Null_Functions' name = 'SQL_Null_Functions'><a href='#SQL_Null_Functions'>SQL Null Functions</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Stored_Procedures' name = 'SQL_Stored_Procedures'><a href='#SQL_Stored_Procedures'>SQL Stored Procedures</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Comments' name = 'SQL_Comments'><a href='#SQL_Comments'>SQL Comments</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Operators' name = 'SQL_Operators'><a href='#SQL_Operators'>SQL Operators</a></li>
-<li class='LeftSideIndex_Btn' id='' name = ''><a href='#'></a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Database' name = 'SQL_Database'><a href='#SQL_Database'>SQL Database</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Create_DB' name = 'SQL_Create_DB'><a href='#SQL_Create_DB'>SQL Create DB</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Drop_DB' name = 'SQL_Drop_DB'><a href='#SQL_Drop_DB'>SQL Drop DB</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Backup_DB' name = 'SQL_Backup_DB'><a href='#SQL_Backup_DB'>SQL Backup DB</a></li>
-
-
-<li class='LeftSideIndex_Btn_h' id='SQL_Not' name = 'SQL_Not'><a href='#SQL_Not'>SQL functions</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Create_Table' name = 'SQL_Create_Table'><a href='#SQL_Create_Table'>SQL Create Table</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Drop_Table' name = 'SQL_Drop_Table'><a href='#SQL_Drop_Table'>SQL Drop Table</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Alter_Table' name = 'SQL_Alter_Table'><a href='#SQL_Alter_Table'>SQL Alter Table</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Constraints' name = 'SQL_Constraints'><a href='#SQL_Constraints'>SQL Constraints</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Not_Null' name = 'SQL_Not_Null'><a href='#SQL_Not_Null'>SQL Not Null</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Unique' name = 'SQL_Unique'><a href='#SQL_Unique'>SQL Unique</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Primary_Key' name = 'SQL_Primary_Key'><a href='#SQL_Primary_Key'>SQL Primary Key</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Foreign_Key' name = 'SQL_Foreign_Key'><a href='#SQL_Foreign_Key'>SQL Foreign Key</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Check' name = 'SQL_Check'><a href='#SQL_Check'>SQL Check</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Default' name = 'SQL_Default'><a href='#SQL_Default'>SQL Default</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Index' name = 'SQL_Index'><a href='#SQL_Index'>SQL Index</a></li>
-
-<li class='LeftSideIndex_Btn_h' id='SQL_Not' name = 'SQL_Not'><a href='#SQL_Not'>SQL DDL</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Auto_Increment' name = 'SQL_Auto_Increment'><a href='#SQL_Auto_Increment'>SQL Auto Increment</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Dates' name = 'SQL_Dates'><a href='#SQL_Dates'>SQL Dates</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Views' name = 'SQL_Views'><a href='#SQL_Views'>SQL Views</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Injection' name = 'SQL_Injection'><a href='#SQL_Injection'>SQL Injection</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Hosting' name = 'SQL_Hosting'><a href='#SQL_Hosting'>SQL Hosting</a></li>
-<li class='LeftSideIndex_Btn' id='SQL_Data_Types' name = 'SQL_Data_Types'><a href='#SQL_Data_Types'>SQL Data Types</a></li>
+<li class='LeftSideIndex_Btn_h' id='SQL DDL' name = 'SQL DDL'>SQL SQL DDL</li>
+<a href='/create_DB.html'><li class='LeftSideIndex_Btn' id='create_DB' name = 'create_DB'>SQL Create Database</li></a>
+<a href='/create_TBL.html'><li class='LeftSideIndex_Btn' id='create_TBL' name = 'create_TBL'>SQL create TABLE</li></a>
+<a href='/ALT_DB.html'><li class='LeftSideIndex_Btn' id='ALT_DB' name = 'ALT_DB'>SQL Alter Database</li></a>
+<a href='/ALT_TBL.html'><li class='LeftSideIndex_Btn' id='ALT_TBL' name = 'ALT_TBL'>SQL Alter Table</li></a>
+<a href='/drop_TBL.html'><li class='LeftSideIndex_Btn' id='drop_TBL' name = 'drop_TBL'>SQL Drop Table</li></a>
+<a href='/drop_DB.html'><li class='LeftSideIndex_Btn' id='drop_DB' name = 'drop_DB'>SQL Drop Database</li></a>
+<a href='/rename.html'><li class='LeftSideIndex_Btn' id='rename' name = 'rename'>SQL Rename</li></a>
+<a href='/truncate.html'><li class='LeftSideIndex_Btn' id='truncate' name = 'truncate'>SQL Truncate</li></a>
     </div>`;
 
-	const navigationbar = `
+		const navigationbar = `
       <nav class='navbar navbar-expand-lg navbar-light bg-light fixed-top'>
         <div class='container-fluid'>
         <button class='navbar-toggler collapsed' type='button' data-bs-toggle='collapse' onclick='toggleNav()' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
@@ -210,7 +147,7 @@ $(document).ready(function() {
       </div>
      </nav>`;
 
-const NavigationbarMobile = `
+		const NavigationbarMobile = `
 <nav class="navbar1 fixed-bottom navbar-light bg-light">
     <div class="container-fluid">
         <a id='navbarSupportedContent'></a>
@@ -249,15 +186,34 @@ const NavigationbarMobile = `
     </div>
 </nav>
 `;
-document.getElementById("navigation_bar").innerHTML = navigationbar;
-var getclass = document.getElementById("getclass").textContent;
-var getclassModified = `id='${getclass}'`;
+const footer = `
+  <div class="row">
+    <div class="col-md-6 text-center text-md-start">
+        <b>Copyright &copy; 2023 <a href="#">Feelit.today</a>, All rights reserved.</b><br>
+    </div>
+    <div class="col-md-6 text-center text-md-end">
+      <a href="#" class='footeropt'>CONTACTUS</a>&nbsp;|&nbsp;
+      <a href="#" class='footeropt'>ABOUTUS</a>&nbsp;|&nbsp;     
+      <a href="#" class='footeropt'>DISCLIMER</a>&nbsp;|&nbsp;
+      <a href="#" class='footeropt'>FOLLOW US</a>&nbsp;|&nbsp;
+      <a href="#" class='footeropt'>HOME</a>&nbsp;|&nbsp;
+      <a href="#" class='footeropt'>COOKIES</a>&nbsp;|&nbsp;
+      <a href="#" class='footeropt'>HELP</a>&nbsp;|&nbsp;
+      <a href="#" class='footeropt'>SUBCRIBE</a>
+    </div>
+  </div>
+ `;
+		document.getElementById("footer").innerHTML = footer;
 
-var listofitemsModified = listofitems.replace(new RegExp(`id='${getclass}'`), `${getclassModified} style='background-color:#04aa6d';`);
-//var bModified = b.replace(new RegExp(`id='${a}'`), `${aModified} class='active'`);
+		document.getElementById("navigation_bar").innerHTML = navigationbar;
+		var getclass = document.getElementById("getclass").textContent;
+		var getclassModified = `id='${getclass}'`;
+
+		var listofitemsModified = listofitems.replace(new RegExp(`id='${getclass}'`), `${getclassModified} style='background-color:#d4dceb;'`);
+		//var bModified = b.replace(new RegExp(`id='${a}'`), `${aModified} class='active'`);
 
 		if ($(window).width() < 450) {
-      document.getElementById("navigation_bar_botom").innerHTML = NavigationbarMobile;
+			document.getElementById("navigation_bar_botom").innerHTML = NavigationbarMobile;
 
 			$('#sidenavbtn').show();
 			$('#laptop').hide();
@@ -279,5 +235,3 @@ var listofitemsModified = listofitems.replace(new RegExp(`id='${getclass}'`), `$
 
 //var getclass = document.getElementById("getclass").textContent;
 //document.getElementById(getclass).style.backgroundColor = "#04aa6d";
-
-
